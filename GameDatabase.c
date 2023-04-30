aff49f5ba9b9d2c85fcd562f5e07a7242ce858bd
lachierigg05 <lachrigg@gmail.com>
2023-05-01 09:03:56 +1000

    Added the game data type

--- GameDatabase.c
+++ GameDatabase.c
@@ -2,15 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_STRING_LENGTH 100
+
+typedef struct {
+    char title[MAX_STRING_LENGTH];
+    int year; 
+    char platform[MAX_STRING_LENGTH];
+    float price;
+} Game; 
+    
+
 void print_welcome_message(); 
 void print_menu(); 
 
 int main() {
     
     print_welcome_message(); 
-    print_menu();
+    
+    int i = 0; 
+    while (i != 1) {
+        print_menu();
+        int selection;
+        scanf("%d", &selection); 
+        
+        switch (selection) {
+            case 1:
+                printf("Nice choice!\n");
+                break; 
+            case 2: 
+                break;
+            case 3:
+                break;
+            case 4:
+                printf("Thanks for using Game Archiver!\n"); 
+                printf("Goodbye!\n"); 
+                return 0; 
+            case 5:
+            default: 
+                printf("Please enter a valid command.\n"); 
+        }
+        getchar(); // To clear the input buffer after a selection is made.
+    }
 
-    printf("\nHello World!\n");
+    printf("\n");
     
     return 0; 
 }
